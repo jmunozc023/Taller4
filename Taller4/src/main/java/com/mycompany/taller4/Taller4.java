f9e1c99c074dda968397f433c163ceb6a5d03c9a
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-17 18:55:26 -0600

    Initial

--- Taller4/src/main/java/com/mycompany/taller4/Taller4.java
+++ Taller4/src/main/java/com/mycompany/taller4/Taller4.java
@@ -1,17 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.taller4;
-
-/**
- *
- * @author josep
- */
-public class Taller4 {
-
+import javax.swing.*;
+import java.awt.*;
+public class Taller4 extends JFrame{
+    private JLabel label1;
+    Taller4(){
+        setLayout(null);
+        label1= new JLabel("Adivina el numero");
+        label1.setBounds(10,20,300,30);
+        add(label1);
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Taller4 juego=new Taller4();
+        juego.setBounds(171,134,1024,500);
+        juego.setResizable(false);
+        juego.setVisible(true);
+        
     }
 }
