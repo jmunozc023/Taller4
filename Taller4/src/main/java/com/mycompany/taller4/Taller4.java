9c8282a5bb37dabb0dde012c3822645670e0c004
jmunozc023 <jmunozc023@ulacit.ed.cr>
2022-07-17 19:25:03 -0600

    Update Taller4.java
    Formulario implementado

--- Taller4/src/main/java/com/mycompany/taller4/Taller4.java
+++ Taller4/src/main/java/com/mycompany/taller4/Taller4.java
@@ -1,16 +1,31 @@
 package com.mycompany.taller4;
 import javax.swing.*;
 import java.awt.*;
-public class Taller4 extends JFrame{
+import java.awt.event.*;
+public class Taller4 extends JFrame implements ActionListener{
+    private JTextField textfield1;
     private JLabel label1;
+    private JButton boton1;
     Taller4(){
         setLayout(null);
         label1= new JLabel("Adivina el numero", SwingConstants.CENTER);
-        label1.setFont(new Font("Serif",Font.BOLD,50));
+        label1.setFont(new Font("Sans",Font.BOLD,50));
         label1.setBounds(5,30,1000,100);
         add(label1);
+        textfield1= new JTextField();
+        textfield1.setBounds(350,150,300,40);
+        add(textfield1);
+        boton1= new JButton("Aceptar");
+        boton1.setBounds(450, 200, 100, 30);
+        add(boton1);
+        boton1.addActionListener(this);
+        
     }
-    
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==boton1){
+            String numero=textfield1.getText();
+        }
+    }
     public static void main(String[] args) {
         Taller4 juego=new Taller4();
         juego.setBounds(171,134,1024,500);
@@ -18,4 +33,6 @@ public class Taller4 extends JFrame{
         juego.setVisible(true);
         
     }
+
+   
 }
